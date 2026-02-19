2e34ca8e73f7a276906c98e98d40256c0b322185
09876-aksh <akshaypujeri636@gmail.com>
2026-02-19 20:22:02 +0530

    solved problems on day 6

--- arrays/BestTimeToButSellStock.java
+++ arrays/BestTimeToButSellStock.java
@@ -1,5 +1,37 @@
 package arrays;
 
 public class BestTimeToButSellStock {
-    
+
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            // Step 1: track minimum buying price
+            if (price < minPrice) {
+                minPrice = price;
+            }
+
+            // Step 2: calculate profit if sold today
+            int profit = price - minPrice;
+
+            // Step 3: update maximum profit
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7,1,5,3,6,4};
+
+        System.out.println(maxProfit(prices));
+    }
 }
+
+
--- arrays/sort012.java
+++ arrays/sort012.java
@@ -1,50 +1,99 @@
 package arrays;
+
+//brute solution
+//time : o(nlogn)
+//space : o(1)
+
 import java.util.Arrays;
 
 public class sort012 {
 
-    public void sortColors(int[] nums) {
-
-        int low = 0;
-        int mid = 0;
-        int high = nums.length - 1;
-
-        while (mid <= high) {
-
-            if (nums[mid] == 0) {
-                swap(nums, low, mid);
-                low++;
-                mid++;
-            }
-            else if (nums[mid] == 1) {
-                mid++;
-            }
-            else { // nums[mid] == 2
-                swap(nums, mid, high);
-                high--;
-            }
-        }
+    public static void sortColors(int[] nums) {
+        Arrays.sort(nums);
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    // 🔥 Main function
-    
     public static void main(String[] args) {
-
-        sort012 sol = new sort012();
 
         int[] nums = {2, 0, 2, 1, 1, 0};
 
-        sol.sortColors(nums);
+        sortColors(nums);
 
         System.out.println("Sorted array: " + Arrays.toString(nums));
     }
 }
+
+//better solution
+//time : o(n)
+//space : o(1)
+
+// import java.util.Arrays;
+
+// public class SortColorsBrute {
+
+//     public static void sortColors(int[] nums) {
+//         Arrays.sort(nums);
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] nums = {2, 0, 2, 1, 1, 0};
+
+//         sortColors(nums);
+
+//         System.out.println("Sorted array: " + Arrays.toString(nums));
+//     }
+// }
+
+// //optimal solution
+// //time : o(n)
+// //space :o(1)
+
+// import java.util.Arrays;
+
+// public class sort012 {
+
+//     public void sortColors(int[] nums) {
+
+//         int low = 0;
+//         int mid = 0;
+//         int high = nums.length - 1;
+
+//         while (mid <= high) {
+
+//             if (nums[mid] == 0) {
+//                 swap(nums, low, mid);
+//                 low++;
+//                 mid++;
+//             }
+//             else if (nums[mid] == 1) {
+//                 mid++;
+//             }
+//             else { // nums[mid] == 2
+//                 swap(nums, mid, high);
+//                 high--;
+//             }
+//         }
+//     }
+
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+
+//     // 🔥 Main function
+    
+//     public static void main(String[] args) {
+
+//         sort012 sol = new sort012();
+
+//         int[] nums = {2, 0, 2, 1, 1, 0};
+
+//         sol.sortColors(nums);
+
+//         System.out.println("Sorted array: " + Arrays.toString(nums));
+//     }
+// }
 
 
 
